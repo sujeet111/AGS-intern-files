85de22a21fb4ff5f6e36570f1442757829b0c39b
Sujeet <64351924+sujeet111@users.noreply.github.com>
2021-06-01 18:52:16 +0530

    Update assinment3.py

--- Sujeet/assinment3.py
+++ Sujeet/assinment3.py
@@ -3,8 +3,10 @@ import pyodbc
 conn = pyodbc.connect('Driver={SQL Server};''Server=DESKTOP-CAFAM2K\SUJEETPATIL;''Database=Agsdb;''Trusted_Connection=yes;')
 
 mycursor = conn.cursor()
-mycursor.execute("INSERT INTO age_diaplay(name , age) Select name, datediff( YY, Date, GETDATE()) as age from bday")
+insertcursor = conn.cursor()
+# mycursor.execute("INSERT INTO age_diaplay(name,age) Select name, datediff( YY, bday.Date, GETDATE()) as age from bday")
 
-mycursor.execute("select * from age_display")
+insertcursor.execute("INSERT INTO age_diaplay(name,age) Select name, datediff( YY, bday.Date, GETDATE()) from bday")
+mycursor.execute("select * from age_diaplay")
 for row in mycursor:
     print(row)
