b271f3ab85847a2997f6dde335478fc829f2b786
gaurav-3963 <patilgaurav345@gmail.com>
2021-06-01 16:53:56 +0530

    Updating database

--- Gaurav/Assignment 2/assignment2.py
+++ Gaurav/Assignment 2/assignment2.py
@@ -8,12 +8,25 @@ Created on Tue Jun  1 14:48:08 2021
 import pyodbc as db
 
 conn = db.connect('Driver={SQL Server};''Server=DESKTOP-VI5MRAI\GAURAVPATIL;''Database=sample;''Trusted_Connection=yes;')
-cursor = conn.cursor()
-cursor.execute("select * into copy_data from sampleinfo")      # Making new table and copying values of previous record
-new_cursor = conn.cursor()                                     # Created new cursor
-new_cursor.execute("select * from copy_data")                  # Displaying copied table data 
+
+c = conn.cursor()
+c.execute("SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'sample_copy'")
+
+if c.fetchone()[0]==1 : {
+	print('Table exists.')
+}
+else:
+    print('Table Dosent exists.')
+    create_table = conn.cursor()
+    create_table.execute("CREATE TABLE sample_copy(ID int ,NAME varchar(50),SURNAME varchar(50));")
+    
+copy = conn.cursor()
+print("Copying data from sample data to copy data")
+copy.execute("INSERT INTO sample_copy(ID,NAME,SURNAME) SELECT ID,NAME,SURNAME FROM sampleinfo ")
+new_cursor = conn.cursor()                                
+new_cursor.execute("select * from sample_copy")                 
 for row in new_cursor:
     print(row)
-print("Over")
+conn.close();
 
           
