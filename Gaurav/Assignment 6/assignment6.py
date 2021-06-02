fa01d2a74a2583667e1055e78ad2d8fa1c503afe
gaurav-3963 <patilgaurav345@gmail.com>
2021-06-02 18:30:00 +0530

    updated ass6

--- Gaurav/Assignment 6/assignment6.py
+++ Gaurav/Assignment 6/assignment6.py
@@ -9,8 +9,6 @@ import pandas as pd
 
 conn = db.connect('Driver={SQL Server};''Server=DESKTOP-VI5MRAI\GAURAVPATIL;''Database=sample;''Trusted_Connection=yes;')
 c = conn.cursor()
-#c.execute("Drop Table Table_1")
-#print("dropped")
 
 def checktable(table_name):
     c = conn.cursor()
@@ -20,7 +18,7 @@ def checktable(table_name):
     except:
         print('Table Dosent exists.\n\n')
         new_table = conn.cursor()
-        new_table.execute("CREATE TABLE "+table_name+"(NAME varchar(50),ID bigint,Price float);");
+        new_table.execute("CREATE TABLE "+table_name+"(NAME varchar(50),ID bigint,Price float,Discount float);");
         new_table.commit()
         new_table.close()
     finally:
@@ -37,37 +35,26 @@ cursor = conn.cursor()
 for row in data.itertuples():
     b = str(row.ID)
     if(b[0]=='4'):
-        a = row.Total_Amount - row.Total_Amount*0.02
-        cursor.execute("INSERT into Table_1 values(?,?,?)",row.NAME,row.ID,a)
+        cursor.execute("INSERT into Table_1 values(?,?,?,?)",row.NAME,row.ID,row.Total_Amount,row.Total_Amount*0.02)
     elif(b[0]=='5'):
-        a = row.Total_Amount - row.Total_Amount*0.03
-        cursor.execute("INSERT into Table_2 values(?,?,?)",row.NAME,row.ID,a)
+        cursor.execute("INSERT into Table_2 values(?,?,?,?)",row.NAME,row.ID,row.Total_Amount,row.Total_Amount*0.03)
     else:
-        a = row.Total_Amount - row.Total_Amount*0.04
-        cursor.execute("INSERT into Table_3 values(?,?,?)",row.NAME,row.ID,a)
+        cursor.execute("INSERT into Table_3 values(?,?,?,?)",row.NAME,row.ID,row.Total_Amount,row.Total_Amount*0.04)
     cursor.commit()
     
 cursor.close()
 
 print("\n\nTable 1")
-c = conn.cursor()
-d = c.execute("select * from Table_1")
-for row in d:
-    print(row)
-d=None
+sql_query = pd.read_sql_query('select * from Table_1',conn)
+print(sql_query)
+
 print("\n\nTable 2")
-d = c.execute("select * from Table_2")
-for row in d:
-    print(row)
-d=None
+sql_query = pd.read_sql_query('select * from Table_2',conn)
+print(sql_query)
+
 print("\n\nTable 3")
-d = c.execute("select * from Table_3")
-for row in d:
-    print(row)
-c.close()
+sql_query = pd.read_sql_query('select * from Table_3',conn)
+print(sql_query)
+
 conn.close()
-
-    
-
-    
-    
+  
