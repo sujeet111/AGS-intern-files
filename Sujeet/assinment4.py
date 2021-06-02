b03deaa65a2535ea9daa14dbc99afadd5a651e36
Sujeet <64351924+sujeet111@users.noreply.github.com>
2021-06-02 13:04:11 +0530

    update

---
+++ Sujeet/assinment4.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pyodbc
+
+
+
+conn = pyodbc.connect('Driver={SQL Server};''Server=DESKTOP-CAFAM2K\SUJEETPATIL;''Database=Agsdb;''Trusted_Connection=yes;')
+mycursor = conn.cursor()
+
+data = pd.read_csv(r'C:\Users\sujeet\Desktop\Projects\AGS\AGS-intern-files\Sujeet\person.csv')
+df = pd.DataFrame(data, columns= ['Name','Country'])
+
+for row in df.itertuples():
+    mycursor.execute('''INSERT INTO csv(name, country)VALUES (?,?)''',row.Name, row.Country)
+mycursor.execute("select * from csv")
+for row in mycursor:
+    print(row)
+conn.commit()
