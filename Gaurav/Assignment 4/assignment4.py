dc07b858807527c4686329a443a7cf430743048a
gaurav-3963 <patilgaurav345@gmail.com>
2021-06-02 12:42:59 +0530

    Ass 4 Updated

--- Gaurav/Assignment 4/assignment4.py
+++ Gaurav/Assignment 4/assignment4.py
@@ -6,26 +6,44 @@ Created on Wed Jun  2 10:57:16 2021
 """
 
 import pandas as pd
-import pyodbc as db
+import pyodbc as db     
 
-data = pd.read_csv("C:\Office\AGS - Internship\AGS-intern-files\Gaurav\Assignment 4\data.txt")
-print(data.ID)
-
-#df = pd.DataFrame(data, columns= ['ID','Name','Surname','Age'])
+data = pd.read_csv("C:\Office\AGS - Internship\AGS-intern-files\Gaurav\Assignment 4\\realestate.csv")
+print(data.city)
 
 conn = db.connect('Driver={SQL Server};''Server=DESKTOP-VI5MRAI\GAURAVPATIL;''Database=sample;''Trusted_Connection=yes;')
+
+c = conn.cursor()
+
+c.execute("SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'Data_Table'")
+print(c)
+
+if c.fetchone()[0]==1 : {
+	print('Table exists.')
+}
+else:
+    print('Table Dosent exists.')
+    c.execute('''CREATE TABLE Data_Table(Address varchar(5000),
+                         City varchar(50),
+                         Bedroom int,
+                         Bathroom int,
+                         Sq_Ft float,
+                         price longint''');
+    c.commit()
+
+c.close()
 
 cursor = conn.cursor()
 
 for row in data.itertuples():
     print(row)
-    cursor.execute("INSERT into sample_copy values("+str(row.ID)+",'"+row.Name+"','"+row.Surname+"',"+str(row.Age)+")")
+    cursor.execute("INSERT into Data_Table values(?,?,?,?,?,?)",row.street,row.city,row.beds,row.baths,row.sq__ft,row.price)
     cursor.commit()
 
 cursor.close()
 
 curso = conn.cursor()
-curso.execute("select * from sample_copy with(nolock)")
+curso.execute("select * from Data_Table with(nolock)")
 for row in curso:
     print(row)
 
