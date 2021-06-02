2f4d2b93496d127f5f2fc7348b3bbf9694d5d0ba
Sujeet <64351924+sujeet111@users.noreply.github.com>
2021-06-02 19:26:31 +0530

    final

--- Sujeet/assinment5.py
+++ Sujeet/assinment5.py
@@ -13,7 +13,9 @@ def checktable(table_name):
         c.execute("SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'"+table_name+"'")
         if c.fetchone()[0]==1 : {
         	print('Table exists.\n\n')
+            
         }    
+        c.execute('truncate table ' + table_name)
     except:
         print('Table Dosent exists.\n\n')
         new_table = conn.cursor()
@@ -29,29 +31,32 @@ checktable("Table_3")
 
 for row in data.itertuples():
     var = str(row.ID)
-    if var[0] == 4:
-        mycursor.execute('INSERT INTO table1(name, id)VALUES (?,?)',row.Name, row.ID)
+    if var[0] == '4':
+        mycursor.execute('INSERT INTO Table_1(name, id)VALUES (?,?)',row.NAME, row.ID)
         mycursor.commit()
-    elif var[0] == 5:
-        mycursor.execute('INSERT INTO table1(name, id)VALUES (?,?)',row.Name, row.ID)
+    elif var[0] == '5':
+        mycursor.execute('INSERT INTO Table_2(name, id)VALUES (?,?)',row.NAME, row.ID)
         mycursor.commit()
-    elif var[0] == 6:
-        mycursor.execute('INSERT INTO table1(name, id)VALUES (?,?)',row.Name, row.ID)
+    else:
+        mycursor.execute('INSERT INTO Table_3(name, id)VALUES (?,?)',row.NAME, row.ID)
         mycursor.commit()
 mycursor.close()
 
+print("Table 1")
 mycursor1 = conn.cursor()
-mycursor1.execute("select * from table_1")
+mycursor1.execute("select * from Table_1")
 for row in mycursor1:
     print(row)
 mycursor1.close()
+print("Table 2")
 mycursor2 = conn.cursor()
-mycursor2.execute("select * from table_2")
+mycursor2.execute("select * from Table_2")
 for row in mycursor2:
     print(row)
 mycursor2.close()
+print("Table 3")
 mycursor3 = conn.cursor()
-mycursor3.execute("select * from table_3")
+mycursor3.execute("select * from Table_3")
 for row in mycursor3:
     print(row)
 mycursor3.close()
---
+++ Sujeet/assinment6.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import pyodbc
+
+conn = pyodbc.connect('Driver={SQL Server};''Server=DESKTOP-CAFAM2K\SUJEETPATIL;''Database=Agsdb;''Trusted_Connection=yes;')
+data = pd.read_csv(r"C:\Users\sujeet\Desktop\Projects\AGS\AGS-intern-files\Sujeet\data.csv")
+mycursor = conn.cursor()
+
+for row in data.itertuples():
+    var = str(row.ID)
+    if var[0] == '4':
+        amount = int(row.Total_Amount) - (int(row.Total_Amount) * 2 / 100)
+        mycursor.execute('INSERT INTO Table_1(NAME, ID, Total_Amount, discount)VALUES (?,?,?,?)',row.NAME, row.ID, amount, 2)
+
+    elif var[0] == '5':
+        amount = int(row.Total_Amount) - (int(row.Total_Amount) * 3 / 100)      
+        mycursor.execute('INSERT INTO Table_2(NAME, ID, Total_Amount, discount)VALUES (?,?,?,?)',row.NAME, row.ID, amount , 3)
+
+    elif var[0] == '6':
+        amount = int(row.Total_Amount) - (int(row.Total_Amount) * 4 / 100)
+        mycursor.execute('INSERT INTO Table_3(NAME, ID, Total_Amount, discount)VALUES (?,?,?,?)',row.NAME, row.ID, amount, 4)
+    else:
+       print("pass")
+mycursor.close()
+
+print("Table 1")
+mycursor1 = conn.cursor()
+mycursor1.execute("select * from Table_1")
+for row in mycursor1:
+    print(row)
+mycursor1.close()
+print("Table 2")
+mycursor2 = conn.cursor()
+mycursor2.execute("select * from Table_2")
+for row in mycursor2:
+    print(row)
+mycursor2.close()
+print("Table 3")
+mycursor3 = conn.cursor()
+mycursor3.execute("select * from Table_3")
+for row in mycursor3:
+    print(row)
+mycursor3.close()
+conn.close()
